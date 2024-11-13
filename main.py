28b3e95462973c2e22a0606fb338d7d730d1c84f
VASILIYKAS <yoyoynv@gmail.com>
2024-11-13 22:52:07 +0500

    Убрал лишние комментарии

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def main():
     cities = ['Лондон', 'SVO', 'Череповец']
 
     params = {
-        'M': '',  # показывать скорость ветра в м/с
-        'n': '',  # узкая версия (только день и ночь)
-        'q': '',  # тихая версия (без текста "Прогноз погоды")
-        'T': '',  # отключить терминальные последовательности (без цветов)
-        'lang': 'ru'  # язык на котором выводится информация
+        'M': '',
+        'n': '',
+        'q': '',
+        'T': '',
+        'lang': 'ru'
     }
 
     for city in cities:
-        url = url_template.format(city)  # Формируем URL
-        response = requests.get(url, params=params)  # Выполняем запрос
+        url = url_template.format(city)
+        response = requests.get(url, params=params)
 
         response.raise_for_status()
         print(response.text)
